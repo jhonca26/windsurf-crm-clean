894eafb2776f50bc6e696c96930b909d142be77f
jhonca26 <tu-email@ejemplo.com>
2025-03-02 01:35:36 +0100

    fix: actualizar configuración de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -34,6 +35,18 @@ export default {
       },
       fontFamily: {
         sans: ['Inter var', 'sans-serif'],
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+      minHeight: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
       },
     },
   },
